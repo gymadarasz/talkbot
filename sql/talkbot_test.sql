f1bda73e5bd06e67dfb361e5b59e084da22316da
Gyula Madarasz <gyula.madarasz@gmail.com>
2020-12-06 23:48:58 +0100

    Update sql dump

--- sql/talkbot_test.sql
+++ sql/talkbot_test.sql
@@ -3,8 +3,8 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Dec 04, 2020 at 11:32 PM
--- Server version: 8.0.22-0ubuntu0.20.04.2
+-- Generation Time: Dec 06, 2020 at 11:47 PM
+-- Server version: 8.0.22-0ubuntu0.20.04.3
 -- PHP Version: 7.4.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -24,14 +24,14 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `ownership`
+-- Table structure for table `content`
 --
 
-CREATE TABLE `ownership` (
+CREATE TABLE `content` (
   `id` int NOT NULL,
-  `table` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `row_id` int NOT NULL,
-  `user_id` int NOT NULL
+  `owner_user_id` int NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `published` tinyint(1) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -42,7 +42,21 @@ CREATE TABLE `ownership` (
 
 CREATE TABLE `script` (
   `id` int NOT NULL,
-  `name` varchar(100) NOT NULL
+  `content_id` int NOT NULL,
+  `talks` enum('robot','human') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `text` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `script_to_script`
+--
+
+CREATE TABLE `script_to_script` (
+  `id` int NOT NULL,
+  `parent_script_id` int NOT NULL,
+  `child_script_id` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -65,27 +79,37 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `email`, `group`, `hash`, `token`, `active`) VALUES
-(9094, 'test1', 'user', 'test', '1', 0),
-(9095, 'test2', 'user', 'test', '2', 0),
-(9096, 'test3', 'user', 'test', '3', 0);
+(14088, 'test1', 'user', 'test', '1', 0),
+(14089, 'test2', 'user', 'test', '2', 0),
+(14090, 'test3', 'user', 'test', '3', 0);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `ownership`
+-- Indexes for table `content`
 --
-ALTER TABLE `ownership`
+ALTER TABLE `content`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `table_row_user` (`table`,`row_id`,`user_id`) USING BTREE;
+  ADD KEY `name` (`name`),
+  ADD KEY `user_id` (`owner_user_id`);
 
 --
 -- Indexes for table `script`
 --
 ALTER TABLE `script`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `name` (`name`);
+  ADD KEY `talks` (`talks`),
+  ADD KEY `script_id` (`content_id`) USING BTREE;
+
+--
+-- Indexes for table `script_to_script`
+--
+ALTER TABLE `script_to_script`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `parent_script_id` (`parent_script_id`),
+  ADD KEY `child_script_id` (`child_script_id`);
 
 --
 -- Indexes for table `user`
@@ -101,22 +125,51 @@ ALTER TABLE `user`
 --
 
 --
--- AUTO_INCREMENT for table `ownership`
+-- AUTO_INCREMENT for table `content`
 --
-ALTER TABLE `ownership`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1185;
+ALTER TABLE `content`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1634;
 
 --
 -- AUTO_INCREMENT for table `script`
 --
 ALTER TABLE `script`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=660;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `script_to_script`
+--
+ALTER TABLE `script_to_script`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9097;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14094;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `content`
+--
+ALTER TABLE `content`
+  ADD CONSTRAINT `content_ibfk_1` FOREIGN KEY (`owner_user_id`) REFERENCES `user` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `script`
+--
+ALTER TABLE `script`
+  ADD CONSTRAINT `script_ibfk_1` FOREIGN KEY (`content_id`) REFERENCES `content` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `script_to_script`
+--
+ALTER TABLE `script_to_script`
+  ADD CONSTRAINT `script_to_script_ibfk_1` FOREIGN KEY (`parent_script_id`) REFERENCES `script` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `script_to_script_ibfk_2` FOREIGN KEY (`child_script_id`) REFERENCES `script` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
